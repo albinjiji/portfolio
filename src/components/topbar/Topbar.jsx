44a0210efb4742fe2f31fe6b8b7f6add2e007c53
Albin <albinjiji4@gmail.com>
2021-09-28 12:33:20 +0530

    Create topbar structure

--- src/components/topbar/Topbar.jsx
+++ src/components/topbar/Topbar.jsx
@@ -1,5 +1,21 @@
 import "./topbar.scss";
 
 export default function Topbar() {
-  return <div className="topbar"></div>;
+  return <div className="topbar">
+    <div className="wrapper">
+      <div className="left">
+        <a href="#intro" className="logo">albin.</a>
+        <div className="itemContainer">
+
+        </div>
+      </div>
+      <div className="right">
+        <div className="humburger">
+          <span className="line1"></span>
+          <span className="line2"></span>
+          <span className="line3"></span>
+        </div> 
+      </div>
+    </div>
+  </div>;
 }
