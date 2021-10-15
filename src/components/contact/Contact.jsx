d33d6019694a35165d170dc4b84a622e2ab45c86
Albin <albinjiji4@gmail.com>
2021-10-15 10:15:12 +0530

    Add emailjs to contact

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -1,14 +1,25 @@
 import "./contact.scss";
 import img from "../images/connect1.jpg";
+import emailjs from "emailjs-com";
 import { useState } from "react";
 
 export default function Contact() {
-  const [message, setMessage] = useState(false);
-
-  const handleSubmit = (e) => {
+  function sendMail(e) {
     e.preventDefault();
-    setMessage(true);
-  };
+
+    emailjs
+      .sendForm(
+        "service_ylbf3ez",
+        "template_gsyultc",
+        e.target,
+        "user_aAf3eqNvw6c6ijt0voayI"
+      )
+      .then((res) => {
+        console.log(res);
+      })
+      .catch((err) => console.log(err));
+  }
+
   return (
     <div className="contact" id="contact">
       <div className="left">
@@ -18,11 +29,10 @@ export default function Contact() {
       </div>
       <div className="right">
         <h2>Contact</h2>
-        <form onSubmit={handleSubmit}>
-          <input type="text" placeholder="Email" />
-          <textarea placeholder="Message"></textarea>
+        <form onSubmit={sendMail}>
+          <input type="text" placeholder="Email" name="email" />
+          <textarea placeholder="Message" name="message"></textarea>
           <button type="submit">Send</button>
-          {message && <span>Thanks,</span>}
         </form>
       </div>
     </div>
